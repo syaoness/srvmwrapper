19b4bdf87992bc47daf6a34a20a10cf0ddf22155
Syao <syao@dotalux.com>
2011-07-20 02:50:03 +0200

    Changes for the git transition

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+echo "Removing old data"
 rm -rf ScummVMwrapper.app
-svn export ../wrapper/ScummVMwrapper.app ./ScummVMwrapper.app
+
+echo "Cloning app bundle"
+ditto ../wrapper/ScummVMwrapper.app ./ScummVMwrapper.app
+
+echo "Adding scummvm executable"
 cp /Applications/ScummVM.app/Contents/MacOS/scummvm ScummVMwrapper.app/Contents/MacOS/
-cp -r ../configtool/ScummVMwrapperConfig/build/Release/ScummVMwrapperConfig.app ScummVMwrapper.app/
+
+echo "Adding config tool"
+ditto ../configtool/ScummVMwrapperConfig/build/Release/ScummVMwrapperConfig.app ScummVMwrapper.app/ScummVMwrapperConfig.app
+
+echo "Removing extra files"
+find ./ -iname .empty -type f -exec rm -f '{}' \;
+
